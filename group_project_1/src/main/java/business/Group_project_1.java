ff9539b52bd4cdd97b6e7f0543d7e718c6302168
Rosy_JR <silverluo24@gmail.com>
2025-03-10 16:47:41 -0400

    新建main

---
+++ group_project_1/src/main/java/business/Group_project_1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package presentation;
+
+/**
+ *
+ * @author silve
+ */
+public class Group_project_1 {
+    
+}
