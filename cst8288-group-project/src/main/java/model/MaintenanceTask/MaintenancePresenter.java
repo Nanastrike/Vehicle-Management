413a4200b19e7de3930e2de10f4aeffa9bec887e
Yen-Yi Hsu <hsu00026@algonquinlive.com>
2025-03-30 22:45:33 -0400

    update

---
+++ cst8288-group-project/src/main/java/model/MaintenanceTask/MaintenancePresenter.java
@@ -0,0 +1,47 @@
+package model.MaintenanceTask;
+
+import view.MaintenanceView;
+import businesslayer.MaintenanceTaskController;
+import java.sql.SQLException;
+import java.util.List;
+
+public class MaintenancePresenter {
+    private final MaintenanceView view;
+    private final VehicleComponentMonitor monitor;
+    private final MaintenanceTaskController taskController;
+    
+    public MaintenancePresenter(MaintenanceView view) throws SQLException {
+        this.view = view;
+        this.monitor = new VehicleComponentMonitor();
+        this.taskController = new MaintenanceTaskController();
+        this.monitor.addObserver(view);
+    }
+    
+    public void displayMaintenanceTasks() throws SQLException {
+        List<MaintenanceTask> tasks = taskController.getAllMaintenanceTasks();
+        view.displayMaintenanceTasks(tasks);
+    }
+    
+    public void displayComponentAlerts() {
+        List<MaintenanceAlert> alerts = monitor.getComponentStatuses().stream()
+            .map(MaintenanceAlert::new)
+            .toList();
+        view.displayComponentAlerts(alerts);
+    }
+    
+    public void monitorMechanicalComponents(String vehicleId, double brakeWear, double wheelWear, double bearingWear) {
+        monitor.monitorMechanicalComponents(vehicleId, brakeWear, wheelWear, bearingWear);
+    }
+    
+    public void monitorElectricalComponents(String vehicleId, double catenaryWear, double pantographWear, double breakerWear) {
+        monitor.monitorElectricalComponents(vehicleId, catenaryWear, pantographWear, breakerWear);
+    }
+    
+    public void monitorEngineDiagnostics(String vehicleId, double engineTemp, double oilPressure, double fuelEfficiency) {
+        monitor.monitorEngineDiagnostics(vehicleId, engineTemp, oilPressure, fuelEfficiency);
+    }
+    
+    public void clearAlerts() {
+        monitor.clearAlerts();
+    }
+} 
