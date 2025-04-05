8643d4357efecc1ab216c98ce3f6cd805f8bedd5
Lucy <xiaoxiyangjsxx@gmail.com>
2025-04-04 23:10:29 -0400

    fix sql

--- cst8288-group-project/src/main/java/Fuel_strategy/BusConsumptionStrategy.java
+++ cst8288-group-project/src/main/java/Fuel_strategy/BusConsumptionStrategy.java
@@ -6,8 +6,8 @@ public class BusConsumptionStrategy implements ConsumptionStrategy {
 
     @Override
     public double calculateConsumption(Vehicle vehicle, double distance) {
-        // 使用 consumptionRate 作为 L/100km 的油耗
-        double rate = vehicle.getConsumptionRate(); // 通常是 float，也可以强转 double
+      
+        double rate = vehicle.getConsumptionRate(); 
         double consumption = rate * distance / 100.0;
         return consumption;
     }
